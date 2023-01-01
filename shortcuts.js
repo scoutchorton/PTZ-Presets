83749b593510784eb32b7aa428232baa4441b87f
scoutchorton <scoutchorton@yahoo.com>
2023-01-01 09:14:48 -0500

    Upload of previous iteration

---
+++ shortcuts.js
@@ -0,0 +1,26 @@
+const shortcuts = {
+	"1": {
+		"0": "Presenter",
+		"1": "Presenter Close-up",
+		"2": "Duplicate of 0",
+		"3": "Stage (wide)",
+		"4": "Presenter (off stage, right stairs)",
+		"5": "Grand Piano Player",
+		"6": "Stage (wide)",
+		"7": "Acoustic Guitar Player",
+		"8": "Presenter (off stage, left stairs)",
+		"9": "Presenter (off stage, middle)"
+	},
+	"2": {
+		"0": "Presenter",
+		"1": "Crowd View (crowd focus)",
+		"2": "Crowd View (stage focus)",
+		"3": "Crowd View (center sanctuary focus)",
+		"4": "Piano Close-up",
+		"5": "Presenter (off stage, left stairs)",
+		"6": "Stage (wide)",
+		"7": "Stage (not as wide)",
+		"8": "Presenter (off stage, right stairs)",
+		"9": "Sound Booth"
+	}
+};
